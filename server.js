d88fc29cc92d4f89746cf603c3bc324986c97ab7
romana2026 <mihail.ursu2018@gmail.com>
2025-01-15 22:31:33 +0200

    Update server.js

--- server.js
+++ server.js
@@ -5,8 +5,8 @@ const app = express();
 app.use(cors());
 
 const PORT = process.env.PORT || 3000;
-//const API_KEY = process.env.API_KEY;
-const API_KEY = "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";
+const API_KEY = process.env.API_KEY;
+//const API_KEY = "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";
 // Middleware для обработки JSON
 app.use(express.json());
                                           // Маршрут по умолчанию
